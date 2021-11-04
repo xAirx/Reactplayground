09ede356c9d9347d46f14fffe873ed782edf8008
xAirx <marcowurtz@hotmail.com>
2021-11-04 21:04:31 +0100

    Init project
    Init project

---
+++ VueVsReact/react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+import Structure from './Structure/Structure'
+
+function App() {
+  return (
+    <div className="App">
+      <Structure />
+    </div>
+  )
+}
+
+export default App
---
+++ VueVsReact/vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <structure />
+  </div>
+</template>
+
+<script>
+import Structure from './Structure/Structure.vue'
+
+export default {
+  name: 'App',
+  components: { Structure },
+}
+</script>
