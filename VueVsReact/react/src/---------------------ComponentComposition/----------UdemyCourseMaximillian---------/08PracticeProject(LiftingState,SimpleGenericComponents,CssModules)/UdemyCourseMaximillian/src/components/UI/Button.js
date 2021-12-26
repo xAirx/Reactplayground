484c4d28f2e5a0ebe9fdf8facbef0e12de99e280
xAirx <marcowurtz@hotmail.com>
2021-12-27 00:20:09 +0100

    up

--- VueVsReact/react/src/---------------------ComponentComposition/----------UdemyCourseMaximillian---------/08PracticeProject(LiftingState,SimpleGenericComponents,CssModules)/UdemyCourseMaximillian/src/components/UI/Button.js
+++ VueVsReact/react/src/---------------------ComponentComposition/----------UdemyCourseMaximillian---------/08PracticeProject(LiftingState,SimpleGenericComponents,CssModules)/UdemyCourseMaximillian/src/components/UI/Button.js
@@ -1,6 +1,8 @@
+import React from "react";
+
 import classes from "./Button.module.css";
 
-const Button = ({ type, onClick, children }) => {
+const Button = ({ type, children, onClick }) => {
   return (
     <button
       className={classes.button}
---
+++ VueVsReact/react/src/---------------------ComponentComposition/----------UdemyCourseMaximillian---------/08PracticeProject(LiftingState,SimpleGenericComponents,CssModules)/UdemyCourseMaximillian/src/components/Users/UsersList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
+
+const UsersList = ({ users }) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
