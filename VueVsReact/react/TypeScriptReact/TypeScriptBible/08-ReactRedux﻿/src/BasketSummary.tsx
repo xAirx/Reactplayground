a6ad4714c43ccfcb74d6b99e3513dde1dbcb912e
xAirx <marcowurtz@hotmail.com>
2022-04-14 04:13:50 +0200

    up

--- VueVsReact/react/TypeScriptReact/TypeScriptBible/08-ReactRedux﻿/src/BasketSummary.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-interface IProps {
-  count: number;
-}
-const BasketSummary: React.FC<IProps> = props => {
-  return <div className="basket-summary">{props.count}</div>;
-};
-export default BasketSummary;
--- VueVsReact/react/TypeScriptReact/TypeScriptBible/11-UnitTesting/01-ReactShop/src/BasketActions.ts
+++
@@ -1,6 +0,0 @@
-import { BasketActionTypes, IBasketAdd } from "./BasketTypes";
-import { IProduct } from "./ProductsData";
-export const addToBasket = (product: IProduct): IBasketAdd => ({
-  product,
-  type: BasketActionTypes.ADD
-});
